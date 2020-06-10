f98cc86a20ed79ddf46e5798c6c10d1d60cfa7c2
Matteus Magnusson <senth.wallace@gmail.com>
2020-06-10 14:48:21 +0200

    ➕Add babel to ts-jest so we can use ?. operator

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-	preset: 'ts-jest',
+	preset: 'ts-jest/presets/js-with-babel',
 	testEnvironment: 'node',
 	roots: ['./src'],
+	globals: {
+		'ts-jest': {
+			babelConfig: true,
+		},
+	},
 }
