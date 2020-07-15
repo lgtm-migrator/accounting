10d366ec919f0eb202a3aede19895a53f82b1a44
Matteus Magnusson <senth.wallace@gmail.com>
2020-07-15 19:37:17 +0200

    🐛 Revert back to correct Promise handling

--- backend/src/framework/file/PdfReader.ts
+++ backend/src/framework/file/PdfReader.ts
@@ -5,22 +5,19 @@ import { InternalError } from '../../app/core/definitions/InternalError'
 
 export class PdfReader implements FileReader {
 	async read(file: string): Promise<string> {
-		const promise = new Promise<string>((resolve) => {
-			let dataBuffer: Buffer
-			try {
-				dataBuffer = readFileSync(file)
-			} catch (error) {
-				throw new InternalError(InternalError.Types.fileNotFound, file)
-			}
+		let dataBuffer: Buffer
+		try {
+			dataBuffer = readFileSync(file)
+		} catch (error) {
+			throw new InternalError(InternalError.Types.fileNotFound, file)
+		}
 
-			return pdfParse(dataBuffer)
-				.then((data) => {
-					return resolve(data.text)
-				})
-				.catch((error) => {
-					throw new InternalError(InternalError.Types.readingFile, error)
-				})
-		})
-		return promise
+		return pdfParse(dataBuffer)
+			.then((data) => {
+				return data.text
+			})
+			.catch((error) => {
+				throw new InternalError(InternalError.Types.readingFile, error)
+			})
 	}
 }
--- backend/src/framework/file/TxtReader.ts
+++ backend/src/framework/file/TxtReader.ts
@@ -4,14 +4,11 @@ import { InternalError } from '../../app/core/definitions/InternalError'
 
 export class TxtReader implements FileReader {
 	async read(file: string): Promise<string> {
-		const promise = new Promise<string>((resolve) => {
-			try {
-				const data = readFileSync(file, 'utf8')
-				resolve(data)
-			} catch (error) {
-				throw new InternalError(InternalError.Types.fileNotFound, error)
-			}
-		})
-		return promise
+		try {
+			const data = readFileSync(file, 'utf8')
+			return data
+		} catch (error) {
+			throw new InternalError(InternalError.Types.fileNotFound, error)
+		}
 	}
 }
