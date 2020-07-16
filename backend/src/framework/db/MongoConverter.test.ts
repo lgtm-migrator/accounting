d2dd3298204fe9d40a142f6efcaa0b3eab9370c0
Matteus Magnusson <senth.wallace@gmail.com>
2020-07-16 18:57:06 +0200

    ✨ Can convert directly to Entity subclass

--- backend/src/framework/db/MongoConverter.test.ts
+++ backend/src/framework/db/MongoConverter.test.ts
@@ -1,8 +1,14 @@
-import { Entity } from '../../app/core/entities/Entity'
 import * as faker from 'faker'
 import { MongoConverter } from './MongoConverter'
 import { InternalError } from '../../app/core/definitions/InternalError'
 import { ObjectId } from 'mongodb'
+import { Currency } from '../../app/core/entities/Currency'
+import { Transaction } from '../../app/core/entities/Transaction'
+import { Verification } from '../../app/core/entities/Verification'
+import { userInfo } from 'os'
+import { Account } from '../../app/core/entities/Account'
+import { ParserSingle } from '../../app/core/entities/ParserSingle'
+import { ParserMulti } from '../../app/core/entities/ParserMulti'
 
 describe('MongoConverter #cold #helper', () => {
 	// toDbObject()
@@ -124,90 +130,397 @@ describe('MongoConverter #cold #helper', () => {
 		}
 	})
 
-	// toEntity()
-	it('toEntity() Test simple object', () => {
-		const entity: any = {
+	it('toDbObject() Regex', () => {
+		const entity = {
+			regex: /hello world/,
+		}
+
+		const object = MongoConverter.toDbObject(entity)
+
+		const valid = {
+			_id: object._id,
+			regex: entity.regex,
+		}
+
+		expect(object).toStrictEqual(valid)
+	})
+
+	it('toDbObject() Currency', () => {
+		const entity = {
+			currency: new Currency({
+				amount: 1n,
+				code: Currency.Codes.SEK,
+			}),
+		}
+
+		const object = MongoConverter.toDbObject(entity)
+
+		const valid = {
+			_id: object._id,
+			currency: {
+				amount: '1n',
+				code: 'SEK',
+			},
+		}
+
+		expect(object).toStrictEqual(valid)
+	})
+
+	it('toDbObject() Transaction', () => {
+		const entity = {
+			transaction: new Transaction({
+				accountNumber: 1000,
+				currency: new Currency({
+					amount: 1n,
+					code: Currency.Codes.SEK,
+				}),
+			}),
+		}
+
+		const object = MongoConverter.toDbObject(entity)
+
+		const valid = {
+			_id: object._id,
+			transaction: {
+				dateCreated: entity.transaction.dateCreated,
+				dateModified: entity.transaction.dateModified,
+				accountNumber: 1000,
+				currency: {
+					amount: '1n',
+					code: 'SEK',
+				},
+			},
+		}
+
+		expect(object).toStrictEqual(valid)
+	})
+
+	// toOption()
+	it('toOption() Test simple object', () => {
+		const object: any = {
 			text: faker.random.words(),
 			number: faker.random.number(),
 		}
 
-		const object = MongoConverter.toEntity(entity)
-		expect(object).toStrictEqual(entity)
+		const entity = MongoConverter.toOption(object)
+		expect(entity).toStrictEqual(object)
 	})
 
-	it('toEntity() Test object hiearchy', () => {
-		const entity = {
+	it('toOption() Test object hiearchy', () => {
+		const object = {
 			text: faker.random.words(),
 			innerObject: {
 				otherText: faker.random.words(),
 			},
 		}
 
-		const object = MongoConverter.toEntity(entity)
+		const entity = MongoConverter.toOption(object)
 
-		expect(object).toStrictEqual(entity)
+		expect(entity).toStrictEqual(object)
 	})
 
-	it('toEntity() Test bigint', () => {
+	it('toOption() Test bigint', () => {
 		const valid = {
 			biggie: 12345678912356546456n,
 		}
-		const entity = {
+		const object = {
 			biggie: `${valid.biggie}n`,
 		}
 
-		const object = MongoConverter.toEntity(entity)
+		const entity = MongoConverter.toOption(object)
 
-		expect(object).toStrictEqual(valid)
+		expect(entity).toStrictEqual(valid)
 	})
 
-	it('toEntity() Test null', () => {
-		const entity = {
+	it('toOption() Test null', () => {
+		const object = {
 			text: '',
 			nothing: null,
 		}
 
-		const object = MongoConverter.toEntity(entity)
+		const entity = MongoConverter.toOption(object)
 
-		expect(object).toStrictEqual(entity)
+		expect(entity).toStrictEqual(object)
 	})
 
-	it('toEntity() Test arrays', () => {
-		const entity = {
+	it('toOption() Test arrays', () => {
+		const object = {
 			array: ['test', '123n'],
 		}
 
-		const object = MongoConverter.toEntity(entity)
+		const entity = MongoConverter.toOption(object)
 
 		const valid = {
 			array: ['test', 123n],
 		}
 
-		expect(object).toStrictEqual(valid)
+		expect(entity).toStrictEqual(valid)
 	})
 
-	it('toEntity() Test ids', () => {
+	it('toOption() Test ids', () => {
 		const valid = {
 			id: new ObjectId().toHexString(),
 			userId: new ObjectId().toHexString(),
 		}
 
-		const entity = {
+		const object = {
 			_id: new ObjectId(valid.id),
 			userId: new ObjectId(valid.userId),
 		}
 
-		const object = MongoConverter.toEntity(entity)
+		const entity = MongoConverter.toOption(object)
 
-		expect(object).toStrictEqual(valid)
+		expect(entity).toStrictEqual(valid)
 	})
 
-	it('toEntity() Test empty object', () => {
+	it('toOption() Test invalid _id', () => {
+		const valid = {
+			_id: new ObjectId().toHexString(),
+			userId: new ObjectId().toHexString(),
+		}
+
+		const object = {
+			_id: valid._id,
+			userId: valid.userId,
+		}
+
+		const entity = MongoConverter.toOption(object)
+
+		expect(entity).toStrictEqual(valid)
+	})
+
+	it('toOption() Test empty object', () => {
 		expect.assertions(1)
 		try {
-			MongoConverter.toEntity({})
+			MongoConverter.toOption({})
 		} catch (exception) {
 			expect(exception).toBeInstanceOf(InternalError)
 		}
+	})
+
+	it('Regexp converter', () => {
+		const entity = {
+			id: new ObjectId().toHexString(),
+			regex: /hello world/,
+		}
+
+		expect(MongoConverter.toOption(MongoConverter.toDbObject(entity))).toStrictEqual(entity)
+	})
+
+	it('toVerification() Minimum verification', () => {
+		const valid = new Verification({
+			id: new ObjectId().toHexString(),
+			date: '2020-01-01',
+			userId: new ObjectId().toHexString(),
+			name: faker.commerce.productName(),
+			type: Verification.Types.TRANSACTION,
+			transactions: [],
+		})
+
+		const object = {
+			_id: new ObjectId(valid.id),
+			dateCreated: valid.dateCreated,
+			dateModified: valid.dateModified,
+			userId: new ObjectId(valid.userId),
+			date: valid.date,
+			name: valid.name,
+			type: 'TRANSACTION',
+			transactions: [],
+		}
+
+		const entity = MongoConverter.toVerification(object)
+
+		expect(entity).toStrictEqual(valid)
+	})
+
+	it('toVerification() Full verification', () => {
+		const option: Verification.Option = {
+			id: new ObjectId().toHexString(),
+			userId: new ObjectId().toHexString(),
+			name: faker.commerce.productName(),
+			internalName: faker.commerce.product(),
+			number: faker.random.number(),
+			date: '2020-01-01',
+			dateFiled: faker.random.number(),
+			dateCreated: faker.random.number(),
+			dateModified: faker.random.number(),
+			dateDeleted: faker.random.number(),
+			type: Verification.Types.TRANSACTION,
+			description: 'A description',
+			totalAmount: {
+				amount: 1n,
+				code: 'SEK',
+			},
+			files: ['hello', 'another file'],
+			invoiceId: new ObjectId().toHexString(),
+			paymentId: new ObjectId().toHexString(),
+			requireConfirmation: true,
+			transactions: [
+				{
+					dateCreated: faker.random.number(),
+					dateModified: faker.random.number(),
+					dateDeleted: faker.random.number(),
+					accountNumber: 2020,
+					currency: {
+						amount: 1n,
+						localAmount: 10n,
+						code: 'USD',
+						localCode: 'SEK',
+						exchangeRate: 10,
+					},
+				},
+				{
+					dateCreated: faker.random.number(),
+					dateModified: faker.random.number(),
+					dateDeleted: faker.random.number(),
+					accountNumber: 4661,
+					currency: {
+						amount: -1n,
+						localAmount: -10n,
+						code: 'USD',
+						localCode: 'SEK',
+						exchangeRate: 10,
+					},
+				},
+			],
+		}
+
+		const valid = new Verification(option)
+
+		const object = {
+			_id: new ObjectId(option.id),
+			userId: new ObjectId(option.userId),
+			name: option.name,
+			internalName: option.internalName,
+			number: option.number,
+			date: '2020-01-01',
+			dateFiled: option.dateFiled,
+			dateCreated: option.dateCreated,
+			dateModified: option.dateModified,
+			dateDeleted: option.dateDeleted,
+			type: Verification.Types.TRANSACTION,
+			description: 'A description',
+			totalAmount: {
+				amount: '1n',
+				code: 'SEK',
+			},
+			files: ['hello', 'another file'],
+			invoiceId: new ObjectId(option.invoiceId),
+			paymentId: new ObjectId(option.paymentId),
+			requireConfirmation: true,
+			transactions: [
+				{
+					dateCreated: option.transactions[0].dateCreated,
+					dateModified: option.transactions[0].dateModified,
+					dateDeleted: option.transactions[0].dateDeleted,
+					accountNumber: 2020,
+					currency: {
+						amount: '1n',
+						localAmount: '10n',
+						code: 'USD',
+						localCode: 'SEK',
+						exchangeRate: 10,
+					},
+				},
+				{
+					dateCreated: option.transactions[1].dateCreated,
+					dateModified: option.transactions[1].dateModified,
+					dateDeleted: option.transactions[1].dateDeleted,
+					accountNumber: 4661,
+					currency: {
+						amount: '-1n',
+						localAmount: '-10n',
+						code: 'USD',
+						localCode: 'SEK',
+						exchangeRate: 10,
+					},
+				},
+			],
+		}
+
+		const entity = MongoConverter.toVerification(object)
+
+		expect(entity).toStrictEqual(valid)
+
+		// Test back and forth
+		expect(MongoConverter.toVerification(MongoConverter.toDbObject(entity))).toStrictEqual(valid)
+	})
+
+	it('Account converter', () => {
+		const account = new Account({
+			id: new ObjectId().toHexString(),
+			userId: new ObjectId().toHexString(),
+			number: 2020,
+			vatAccount: 2449,
+			reverseVatAccount: 2555,
+			vatCode: 15,
+			vatPercentage: 0.25,
+		})
+
+		expect(MongoConverter.toAccount(MongoConverter.toDbObject(account))).toStrictEqual(account)
+	})
+
+	it('Single parser converter', () => {
+		const parser = new ParserSingle({
+			id: new ObjectId().toHexString(),
+			name: 'Test parser',
+			identifier: /test/,
+			userId: new ObjectId().toHexString(),
+			verification: {
+				name: 'Name',
+				internalName: 'INTERNAL_NAME',
+				type: Verification.Types.INVOICE_IN,
+				accountFrom: 2499,
+				accountTo: 4330,
+			},
+			matcher: {
+				date: {
+					find: /\d{4}-\d{2}-\d{2}/,
+				},
+				currencyCode: {
+					find: /(?<=code: )\w{3}/,
+				},
+				amount: {
+					find: /(?<=total: )\d{1,4}/,
+				},
+			},
+		})
+
+		expect(MongoConverter.toParser(MongoConverter.toDbObject(parser))).toStrictEqual(parser)
+	})
+
+	it('Multiple parser converter', () => {
+		const parser = new ParserMulti({
+			id: new ObjectId().toHexString(),
+			userId: new ObjectId().toHexString(),
+			name: 'Skattekonto',
+			identifier: /Omfattar transaktionstyp/,
+			accountFrom: 1630,
+			currencyCodeDefault: 'SEK',
+			matcher: /(?<year>\d{2})(?<month>\d{2})(?<day>\d{2})\s+(?<name>.*?)\s{4}\s+(?<amount>-?\d*?\s?\d*)\s{4}/g,
+			lineMatchers: [
+				{
+					identifier: /Debiterad preliminärskatt/,
+					internalName: 'TAX_ACCOUNT_PRELIMINARY_TAX',
+					type: Verification.Types.TRANSACTION,
+					accountTo: 2518,
+				},
+				{
+					identifier: /Moms/,
+					internalName: 'TAX_ACCOUNT_TAX_COLLECT',
+					type: Verification.Types.TRANSACTION,
+					accountTo: 1650,
+				},
+				{
+					identifier: /ostnadsränta/,
+					internalName: 'TAX_ACCOUNT_INTEREST_EXPENSE',
+					type: Verification.Types.TRANSACTION,
+					accountTo: 8423,
+					currencyCodeDefault: 'USD',
+				},
+			],
+		})
+
+		expect(MongoConverter.toParser(MongoConverter.toDbObject(parser))).toStrictEqual(parser)
 	})
 })
--- backend/src/framework/db/MongoConverter.ts
+++ backend/src/framework/db/MongoConverter.ts
@@ -1,7 +1,11 @@
-import { Entity } from '../../app/core/entities/Entity'
 import { ObjectId } from 'mongodb'
 import { InternalError } from '../../app/core/definitions/InternalError'
-import { type } from 'os'
+import { Account } from '../../app/core/entities/Account'
+import { Verification } from '../../app/core/entities/Verification'
+import { Parser } from '../../app/core/entities/Parser'
+import { User } from '../../app/core/entities/User'
+import { ParserSingle } from '../../app/core/entities/ParserSingle'
+import { ParserMulti } from '../../app/core/entities/ParserMulti'
 
 export namespace MongoConverter {
 	/**
@@ -48,11 +52,22 @@ export namespace MongoConverter {
 					object[key] = `${value}n`
 				}
 
-				// Recursive object
-				else if (typeof value === 'object') {
-					const child = serialize(value!)
-					if (child) {
-						object[key] = child
+				// Object
+				else if (typeof value === 'object' && value) {
+					// Currency code
+					if (isCode(value)) {
+						object[key] = (value as any).name
+					}
+					// Regexp - Use value directly
+					else if (value instanceof RegExp) {
+						object[key] = value
+					}
+					// Recursive object
+					else {
+						const child = serialize(value!)
+						if (child) {
+							object[key] = child
+						}
 					}
 				}
 
@@ -67,20 +82,72 @@ export namespace MongoConverter {
 	}
 
 	/**
-	 * Deserialize a Mongo DB object to an entity.
+	 * Check if the object is of type code
+	 * @param object the object to test
+	 * @return true if this is of type code, false otherwise
+	 */
+	function isCode(object: any): boolean {
+		if (!object) {
+			return false
+		}
+		if (!object.name || typeof object.name !== 'string') {
+			return false
+		}
+		if (!object.precision || typeof object.precision !== 'number') {
+			return false
+		}
+
+		return true
+	}
+
+	/**
+	 * Deserialize a Mongo DB object to an entity option.
 	 * Automatically converts bigint, and Mongo db's internal id to string ids.
 	 * The _id property is mapped to id.
 	 * @param object the DB object to serialize to an entity
-	 * @return the serialized object
+	 * @return the deserialized object into an entity option
 	 * @throws {InternalError.Types.dbError} if the object is empty
 	 */
-	export function toEntity<EntityType extends Entity>(object: {}): EntityType {
+	export function toOption(object: {}): any {
 		if (!object || Object.keys(object).length === 0) {
 			throw new InternalError(InternalError.Types.dbError, 'toEntity() empty object')
 		}
 
-		return deserialize(object) as EntityType
+		return deserialize(object)
 	}
+
+	export function toAccount(object: {}): Account {
+		const entityOption = toOption(object)
+		return new Account(entityOption)
+	}
+
+	export function toVerification(object: {}): Verification {
+		const entityOption = toOption(object)
+		return new Verification(entityOption)
+	}
+
+	export function toParser(object: {}): Parser {
+		const entityOption = toOption(object)
+
+		switch (entityOption.type) {
+			case Parser.Types.single:
+				return new ParserSingle(entityOption)
+			case Parser.Types.multi:
+				return new ParserMulti(entityOption)
+			default:
+				throw new InternalError(
+					InternalError.Types.notImplemented,
+					`Parser type ${entityOption.type} not implementd in MongoConverter`
+				)
+		}
+	}
+
+	export function toUser(object: {}): User {
+		const entityOption = toOption(object)
+		return new User(entityOption)
+	}
+
+	// TODO fiscal year
 
 	function deserialize(object: {}): {} {
 		const entity: any = object instanceof Array ? [] : {}
@@ -100,9 +167,16 @@ export namespace MongoConverter {
 				entity[key] = BigInt(value.substr(0, value.length - 1))
 			}
 
-			// Recursive object
+			// Object
 			else if (typeof value === 'object' && value) {
-				entity[key] = deserialize(value)
+				// Regexp - use value directly
+				if (value instanceof RegExp) {
+					entity[key] = value
+				}
+				// Recursive object
+				else {
+					entity[key] = deserialize(value)
+				}
 			}
 
 			// Use value
