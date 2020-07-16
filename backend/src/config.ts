a701111e998924535c9cd848021d736a1defb5e1
Matteus Magnusson <senth.wallace@gmail.com>
2020-07-16 14:55:14 +0200

    ♻ url() helper for mongoDb
    All other info is stored seperately

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -9,8 +9,33 @@ if (process.env.NODE_ENV === 'development') {
 	throw new Error('Could not load config file')
 }
 
+export function mongoDbUrl(config: Config): string {
+	let { host, username, password, database, port } = config.mongoDb
+
+	let credentials = ''
+	if (username && password) {
+		credentials = `${username}:${password}@`
+	}
+
+	let portString = ''
+	if (port) {
+		portString = `:${port}`
+	}
+
+	return `mongodb://${credentials}${host}${portString}/${database}`
+}
+
 export interface Config {
 	mongoDb: {
-		url: string
+		host: string
+		username?: string
+		password?: string
+		database: string
+		port?: number
+
+		/**
+		 * Generate the URL from the info
+		 */
+		url(): string
 	}
 }
