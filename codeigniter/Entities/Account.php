4dd480aa0d1031193a08ca4129b5da6d992f1b1a
Matteus Magnusson <senth.wallace@gmail.com>
2020-07-27 12:25:00 +0200

    🔥 Codeigniter removal

--- codeigniter/Entities/Account.php
+++
@@ -1,5 +0,0 @@
-<?php namespace App\Entities;
-
-class Account extends Entity {
-	
-}
--- codeigniter/Views/login.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('template') ?>
-
-<?= $this->section('content') ?>
-
-<h1>Login Page</h1>
-
-<?= $this->endSection() ?>
